d4d1da2e1cbc19d9d65e1a8f63577565fc301b7e
zhaonan666 <zhaonan.chen@momenta.ai>
2026-07-14 18:30:22 +0800

    nextjs前端

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,67 +1,29 @@
 "use client";
-
+import { Input, Button } from "antd";
 import { useState } from "react";
 
-type HealthResponse = {
-  status: string;
-};
+import { PlusOutlined } from "@ant-design/icons";
 
 export default function Home() {
-  const [status, setStatus] = useState("尚未检查");
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  async function checkBackend() {
-    setIsLoading(true);
-    setError("");
-
-    try {
-      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-      const response = await fetch(`${apiUrl}/health`);
-
-      if (!response.ok) {
-        throw new Error(`请求失败：${response.status}`);
-      }
-
-      const data: HealthResponse = await response.json();
-      setStatus(data.status);
-    } catch (requestError) {
-      const message =
-        requestError instanceof Error ? requestError.message : "未知错误";
-
-      setError(message);
-      setStatus("连接失败");
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  const [messages, setMessages] = useState([
+    { role: "ai", content: "你好" },
+    { role: "human", content: "你是什么模型?" },
+  ]);
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-100 p-6">
-      <section className="w-full max-w-md rounded-2xl bg-white p-8 shadow-sm">
-        <h1 className="text-3xl font-semibold text-zinc-900">My Agent</h1>
+    <div>
+      <div>
+        {messages.map((item, index) => {
+          return <div>{item.content}</div>;
+        })}
+      </div>
 
-        <p className="mt-2 text-zinc-600">
-          后端状态：
-          <span className="ml-2 font-medium">{status}</span>
-        </p>
-
-        <button
-          type="button"
-          onClick={checkBackend}
-          disabled={isLoading}
-          className="mt-6 rounded-lg bg-black px-4 py-2 text-white disabled:cursor-not-allowed disabled:opacity-50"
-        >
-          {isLoading ? "检查中..." : "检查后端连接"}
-        </button>
-
-        {error && (
-          <p className="mt-4 rounded-lg bg-red-50 p-3 text-sm text-red-700">
-            {error}
-          </p>
-        )}
-      </section>
-    </main>
+      <div className="flex flex-col">
+        <Input placeholder="要求后续变更" variant="borderless" />
+        <div>
+          <PlusOutlined />
+        </div>
+      </div>
+    </div>
   );
 }
